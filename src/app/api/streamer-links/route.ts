8d82935555a23ff9dc6b51d70baa4c3ef0932c0e
Teki <64394038+tekigg@users.noreply.github.com>
2024-06-29 03:51:50 +0300

    chore: Update Twitch API requests in streamer-links/route.ts

--- src/app/api/streamer-links/route.ts
+++ src/app/api/streamer-links/route.ts
@@ -1,66 +1,87 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
     const { streamerIds } = await request.json();
 
     if (!Array.isArray(streamerIds)) {
-      return NextResponse.json({ error: 'Invalid input: streamerIds must be an array' }, { status: 400 });
+      return NextResponse.json(
+        { error: "Invalid input: streamerIds must be an array" },
+        { status: 400 }
+      );
     }
 
     // Fetch the Twitch homepage to get the clientId
-    const homeResponse = await fetch('https://www.twitch.tv/', {
+    const homeResponse = await fetch("https://www.twitch.tv/", {
       headers: {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
-      }
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+      },
     });
     const homeData = await homeResponse.text();
 
     const start = 'clientId="';
     const end = '",commonOptions=';
-    const clientId = homeData.substring(homeData.indexOf(start) + start.length, homeData.lastIndexOf(end)).trim();
+    const clientId = homeData
+      .substring(
+        homeData.indexOf(start) + start.length,
+        homeData.lastIndexOf(end)
+      )
+      .trim();
 
-    const url = 'https://gql.twitch.tv/gql';
+    const url = "https://gql.twitch.tv/gql";
 
-    const results = await Promise.all(streamerIds.map(async (streamerId: string) => {
-      const payload = [{
-        "operationName": "ChannelRoot_AboutPanel",
-        "variables": {
-          "channelLogin": streamerId,
-          "skipSchedule": true
-        },
-        "extensions": {
-          "persistedQuery": {
-            "version": 1,
-            "sha256Hash": "6089531acef6c09ece01b440c41978f4c8dc60cb4fa0124c9a9d3f896709b6c6"
-          }
-        }
-      }];
+    const results = await Promise.all(
+      streamerIds.map(async (streamerId: string) => {
+        const payload = [
+          {
+            operationName: "ChannelRoot_AboutPanel",
+            variables: {
+              channelLogin: streamerId,
+              skipSchedule: true,
+            },
+            extensions: {
+              persistedQuery: {
+                version: 1,
+                sha256Hash:
+                  "6089531acef6c09ece01b440c41978f4c8dc60cb4fa0124c9a9d3f896709b6c6",
+              },
+            },
+          },
+        ];
 
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-          'Client-Id': clientId,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-      });
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "user-agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+            "Client-Id": clientId,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
 
-      const data = await response.json();
-      
-      // Add null checks and provide default values
-      const socials = data[0]?.data?.user?.channel?.socialMedias?.map((x: { url: string; name: string}) => ({
-        url: x.url,
-        name: x.name,
-      })) || [];
+        const data = await response.json();
 
-      return { streamerId, socials };
-    }));
+        // Add null checks and provide default values
+        const socials =
+          data[0]?.data?.user?.channel?.socialMedias?.map(
+            (x: { url: string; name: string }) => ({
+              url: x.url,
+              name: x.name,
+            })
+          ) || [];
+        console.log(results, data, response, homeData, clientId, url, payload);
 
+        return { streamerId, socials };
+      })
+    );
     return NextResponse.json(results);
   } catch (error) {
-    console.error('Error:', error);
-    return NextResponse.json({ error: 'An error occurred while fetching streamer information' }, { status: 500 });
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "An error occurred while fetching streamer information" },
+      { status: 500 }
+    );
   }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,13 @@ export const metadata: Metadata = {
   title: "ArensLiveSMP",
   description: "Your favorite SMP, now live!",
   icons: [{ rel: "icon", url: "/assets/icons/favicon.png" }],
+  twitter: {
+    site: "@arens_live",
+    card: "summary_large_image",
+    images: [{ url: "/assets/icons/og.png", width: 1200, height: 630 }],
+  },
   openGraph: {
+    
   locale: 'en_US',
     title: "ArensLiveSMP",
     description: "Your favorite SMP, now live!",
